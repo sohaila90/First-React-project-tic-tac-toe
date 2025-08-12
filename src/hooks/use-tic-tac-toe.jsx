6c224cd0fe6c404620e7fa4ba0370eccb695bdd6
sohaila90 <sohaila.al-khayat@hotmail.com>
2025-08-12 14:56:44 +0200

    Update use-tic-tac-toe.jsx

--- src/hooks/use-tic-tac-toe.jsx
+++ src/hooks/use-tic-tac-toe.jsx
@@ -5,11 +5,24 @@ const initialBoard = () => Array(9).fill(null);
 const useTicTacToe = () => {
   const [board, setBoard] = useState(initialBoard());
 
-  const [isXNext, setIsNext] = useState(true);
+  const [isXNext, setIsXNext] = useState(true);
 
-  const WINNING_PATTERNS = [];
+  const WINNING_PATTERNS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  const calculateWinner = (currentBoard) => {};
+  const calculateWinner = (currentBoard) => {
+    for(let i = 0; i < WINNING_PATTERNS.length; i++) {
+      WINNING_PATTERNS[i]
+    }
+  };
 
   const handleClick = (index) => {
     // check winner
@@ -19,7 +32,7 @@ const useTicTacToe = () => {
     const newBoard = [...board];
     newBoard[index] = isXNext ? "X" : "0";
     setBoard(newBoard);
-    setIsNext(!isXNext);
+    setIsXNext(!isXNext);
   };
 
   function getStatusMessage() {}
