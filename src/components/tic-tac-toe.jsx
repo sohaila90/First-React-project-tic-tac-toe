588bf6d9cc5652d09964554a2bcf075b5a9a2ede
sohaila90 <sohaila.al-khayat@hotmail.com>
2025-08-12 14:28:33 +0200

    test

---
+++ src/components/tic-tac-toe.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import useTicTacToe from "../hooks/use-tic-tac-toe";
+
+function TicTacToe() {
+
+    const {board, calculateWinner, handleClick, getStatusMessage, resetGame} = useTicTacToe();
+
+  return (
+    <>
+      <div className="game">
+        <div className="status">
+          {getStatusMessage()}
+          <button className="reset-button" onClick={resetGame()}>Reset Game</button>
+        </div>
+        
+        <div className="board">
+          {board.map((b,index)=>{
+            return <button className="cell" key={index} onClick={handleClick(index)}
+            disabled={b !== null}
+            >
+            
+ 
+              {b}
+              </button>
+          })}
+          </div>
+      </div>
+    </>
+  );
+}
+export default TicTacToe;
---
+++ src/hooks/use-tic-tac-toe.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const initialBoard = () => Array(9).fill(null);
+
+const useTicTacToe = () => {
+  const [board, setBoard] = useState(initialBoard());
+
+  const [isXNext, setIsNext] = useState(true);
+
+  const WINNING_PATTERNS = [];
+
+  const calculateWinner = (currentBoard) => {};
+
+  const handleClick = (index) => {
+    // check winner
+    const winner = calculateWinner(board);
+    if (winner || board[index]) return;
+
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? "X" : "0";
+    setBoard(newBoard);
+    setIsNext(!isXNext);
+  };
+
+  function getStatusMessage() {}
+
+  const resetGame = () => {};
+
+  return { board, calculateWinner, handleClick, getStatusMessage, resetGame };
+};
+
+export default useTicTacToe;
